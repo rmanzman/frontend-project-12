397f80dcefd62a98936c90bcf6f2474ca6cb33bd
Roman Kraizman <roman.krayzman@gmail.com>
2024-03-24 20:42:56 +0300

    Added intermediate localization

---
+++ frontend/src/locales/en.js
@@ -0,0 +1,13 @@
+const enTranslation = {
+  translation: {
+    enter: 'Enter',
+    username: 'Your username',
+    password: 'Password',
+    required: 'Required',
+    invalidData: 'Invalid user name or password',
+    noAccount: 'No account?',
+    signup: 'Signup',
+  },
+};
+
+export default enTranslation;
---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,13 @@
+const ruTranslation = {
+  translation: {
+    enter: 'Войти',
+    username: 'Ваш ник',
+    password: 'Пароль',
+    required: 'Обязательное поле',
+    invalidData: 'Неверные имя пользователя или пароль',
+    noAccount: 'Нет аккаунта?',
+    signup: 'Регистрация',
+  },
+};
+
+export default ruTranslation;
